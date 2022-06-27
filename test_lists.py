ad7a984d1a38d213464badc1badec587b6ccfa68
John Doe <108037999+offsetlizard@users.noreply.github.com>
2022-06-27 12:28:22 -0700

    add dif test

--- test_lists.py
+++ test_lists.py
@@ -8,9 +8,9 @@ from unittest import TestCase
 class TestLists(TestCase):
 
     def test_is_dog_in_our_list_after_removing_doge_equals_false(self):
-        our_list = ["doge", "house", "grass", "dog", "tree"]
-        our_list.remove("doge")
-        is_item_in_list = "doge" in our_list
+        doge_list = ["doge", "house", "grass", "dog", "tree"]
+        doge_list.remove("doge")
+        is_item_in_list = "doge" in doge_list
         self.assertEqual(False, is_item_in_list)
 
 
@@ -19,3 +19,10 @@ class TestLists(TestCase):
         boomer_set.discard("boomer")
         is_item_in_set = "boomer" in boomer_set
         self.assertEqual(False, is_item_in_set)
+        
+    def test_dif_set_with_two_lists_of_three_items_that_are_not_in_common_equals_three_different_items(self):
+        egg_dog_set = {"egg", "dog", "set1"}
+        big_doge_set = {"big", "doge", "set2"}
+        dif_set = egg_dog_set.difference(big_doge_set)
+        total_items = len(dif_set)
+        self.assertEqual(3, total_items)
