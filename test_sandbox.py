8879a396c6af436ddb9c8829c0be1782708d1b1f
John Doe <108037999+offsetlizard@users.noreply.github.com>
2022-06-27 12:10:32 -0700

    add sandbox

---
+++ test_sandbox.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+# Methods can go up here
+
+
+# Tests can go down here
+class TestSandbox(TestCase):
+
+    def test_two_equals_two(self):
+        number = 2
+        self.assertEqual(2, number)
