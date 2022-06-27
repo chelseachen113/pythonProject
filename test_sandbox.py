998a3dd010901e0d9e7b6e174ccd2df8c7394bbe
John Doe <108037999+offsetlizard@users.noreply.github.com>
2022-06-27 15:27:20 -0700

    add exercises to test_sandbox

--- test_sandbox.py
+++ test_sandbox.py
@@ -10,3 +10,28 @@ class TestSandbox(TestCase):
     def test_two_equals_two(self):
         int_number = 2
         self.assertEqual(2, int_number)
+
+    # 1 Write a test that:
+    # 1. multiply two integer variables
+    # 2. store the result to an integer variable
+    # 3. asserts the output is 45
+
+
+
+
+    # 2 Write a test that:
+    # 1. creates a string variable with the contents "doge"
+    # 2. store the second letter to a string variable
+    # 3. asserts the second string variable is "o"
+
+
+
+
+
+
+    # 3 Write a test that:
+    # 1. creates a list variable with 4 items
+    # 2. store the length of the list to an integer variable
+    # 3. asserts the second integer variable is 4
+
+
