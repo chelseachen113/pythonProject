1fcdfc546c89e3f6da6e4a32f8e8f2808d5704e0
chelsea <106779055+chelseachen113@users.noreply.github.com>
2022-06-30 11:55:22 -0700

    FINSIHEEEEEEEEE

--- test_tuples.py
+++ test_tuples.py
@@ -6,12 +6,11 @@ from unittest import TestCase
 class TestTuples(TestCase):
 
     def test_tuple_four_multiplied_by_nine_equals_thirty_six(self):
-        tpl_factor = (4)
-        tpl_factor_2 = (9)
-        tpl_product = (tpl_factor * tpl_factor_2)
-        self.assertEqual( 36, tpl_product )
+        tpl_numbers = (4, 7, 8, 1)
+        int_total = len(tpl_numbers)
+        self.assertEqual(4, int_total)
 
     def test_tuple_length_of_variable_equals_three(self):
         tpl_phrase = ("blue", "red", "green")
-        tpl_total = len( tpl_phrase )
-        self.assertEqual( 3, tpl_total )
+        int_total = len( tpl_phrase )
+        self.assertEqual( 3, int_total )
